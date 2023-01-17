6fd319c714f65eff42987531fdf2b885cbc02c37
Jayakrishna K J <2arunimaraju@gmail.com>
2023-01-17 19:59:00 +0530

    initial

---
+++ cal.js
@@ -0,0 +1,13 @@
+function displaydata(data){
+    result.value+=data
+}
+function allclear(){
+    result.value=""
+}
+function evaluateexp(){
+    result.value=eval(result.value)
+}
+function backspace(){
+    data=result.value
+    result.value=data.slice(0,-1)
+}
